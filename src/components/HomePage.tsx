ff48fa47a02dca600935f301a4c62c8f32e8fac4
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-01 22:10:01 +0800

    Updates styling and adds buttons to home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import mainImg from '../img/home-img.jpg';
+import { BsArrowRight } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
+    <div className='homepage-container'>
       <img src={mainImg} alt='main-img' className='home-img' />
-      {/* Photo by Roman Ska */}
+      <div className='buttons'>
+        <Link to='about/'>
+          <button>
+            Learn more about our team <BsArrowRight />
+          </button>
+        </Link>
+        <Link to='shop/'>
+          <button>
+            Check out the goods <BsArrowRight />
+          </button>
+        </Link>
+      </div>
+      <p className='img-credit'>
+        <a href='https://www.pexels.com/@roman-ska-3453281/'>
+          Photo by Roman Ska
+        </a>
+      </p>
     </div>
   );
 };
