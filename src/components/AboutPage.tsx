b82a0d9672150d67a9dd239b2469c81b6fc4cf1f
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-02 13:22:09 +0800

    Adds styling to About Page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import aboutImg from '../img/about-img.jpg';
 
 const AboutPage: React.FC = () => {
   return (
-    <div className='page-body'>
-      <h2>About Us</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. In nesciunt
-        explicabo iure cumque perferendis quos quidem voluptatem est soluta sint
-        quo recusandae magni obcaecati, hic consequuntur voluptas nisi ab unde
-        provident repellendus illum. Est debitis ducimus perspiciatis quas.
-        Accusamus, nesciunt! Eius consequatur et tempora architecto expedita
-        perferendis, vero optio quam.
-      </p>
-
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-        consequuntur dolorem nobis vero laborum, consequatur error repudiandae
-        eum numquam, porro voluptate reiciendis atque, deserunt necessitatibus?
+    <div>
+      <img src={aboutImg} alt='about-img' className='home-img' />
+      <div className='about-body'>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In nesciunt
+          explicabo iure cumque perferendis quos quidem voluptatem est soluta
+          sint quo recusandae magni obcaecati, hic consequuntur voluptas nisi ab
+          unde provident repellendus illum. Est debitis ducimus perspiciatis
+          quas. Accusamus, nesciunt! Eius consequatur et tempora architecto
+          expedita perferendis, vero optio quam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
+          consequuntur dolorem nobis vero laborum, consequatur error repudiandae
+          eum numquam, porro voluptate reiciendis atque, deserunt
+          necessitatibus?
+        </p>
+      </div>
+      <p className='img-credit'>
+        <a href='https://www.pexels.com/photo/white-motor-scooter-near-open-door-219095/'>
+          Photo by mali maeder
+        </a>
       </p>
     </div>
   );
