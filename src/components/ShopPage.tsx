e991467176edb2be2cac2e0faeba4f3cba108b86
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-03 19:50:35 +0800

    Adds ability to read products.json and display items (without styling) in ShopPage component

--- src/components/ShopPage.tsx
+++ src/components/ShopPage.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import products from './../data/products.json';
+
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+  img: string;
+  purchaseAt: string;
+  tags: string[];
+}
 
 const ShopPage: React.FC = () => {
+  const [getProducts, setProducts] = useState<Product[]>([]);
+  const [productsAreLoading, setProductsAreLoading] = useState<boolean>(false);
+
+  useEffect((): void => {
+    const loadProducts = async () => {
+      setProductsAreLoading(true);
+      setProducts(products);
+      setProductsAreLoading(false);
+    };
+    loadProducts();
+  }, []);
+
+  console.log(getProducts);
+
   return (
     <motion.div
       key={new Date().getTime()}
@@ -11,7 +35,14 @@ const ShopPage: React.FC = () => {
       className='homepage-container'
     >
       <div data-testid='shop-container' className='shop-container'>
-        Shop
+        {products.map((product) => {
+          return (
+            <div key={product.id} className='product-card'>
+              <div>{product.name}</div>
+              <img src={product.img} alt={product.name} />
+            </div>
+          );
+        })}
       </div>
     </motion.div>
   );
