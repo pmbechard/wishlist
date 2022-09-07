426ec2e1cd3418530e53a7aa4173846c4cf0ce77
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-07 21:51:18 +0800

    Adjusts ProductCard font sizes and length cutoff for product names

--- src/components/Shop/ShopPage/ProductCard.tsx
+++ src/components/Shop/ShopPage/ProductCard.tsx
@@ -10,12 +10,16 @@ interface Props {
 const ProductCard: React.FC<Props> = ({ product, handleProductView }) => {
   return (
     <div className='product-card' id={`pc-${product.id}`}>
-      <h3>{product.name}</h3>
+      <h1>
+        {product.name.length < 35
+          ? product.name
+          : `${product.name.slice(0, 40)}...`}
+      </h1>
       <div className='product-img-container'>
         <img src={product.img} alt={product.name} />
       </div>
       <div className='product-footer'>
-        <h4>{product.price}USD</h4>
+        <h2>{product.price}USD</h2>
         <ProductBtnRibbon
           product={product}
           handleProductView={handleProductView}
