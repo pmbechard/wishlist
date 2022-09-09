db9a1c0bdc8a82876f1a0ac014f58e826af33af6
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-09 21:49:51 +0800

    Updates package.json for deployment to gh-pages

--- src/__tests__/NavBar.test.tsx
+++ src/__tests__/NavBar.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NavBar from '../components/NavBar';
