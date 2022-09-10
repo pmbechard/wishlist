f1b04172439d92fcec740084508014eeb07adab9
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-10 19:04:00 +0800

    Adds styling to cart item counter

--- src/components/Shop/Cart/FloatingCart.tsx
+++ src/components/Shop/Cart/FloatingCart.tsx
@@ -22,7 +22,24 @@ const FloatingCart: React.FC<Props> = ({
       }}
     >
       {getInCart.length > 0 && (
-        <g className='cart-counter'>{getInCart.length}</g>
+        <div
+          id='cart-item-counter'
+          style={{
+            backgroundColor: 'red',
+            position: 'absolute',
+            left: '-3px',
+            top: '-3px',
+            paddingBottom: '1px',
+            paddingRight: '1px',
+            height: '15px',
+            width: '15px',
+            textAlign: 'center',
+            borderRadius: '50%',
+            color: '#eee',
+          }}
+        >
+          {getInCart.length}
+        </div>
       )}
       <BsFillCartFill className='cart-icon' />
     </div>
